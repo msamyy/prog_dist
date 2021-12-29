f9469b28978ac482bd6185f2f25481690bd44097
mouri <hd_mouri@esi.dz>
2021-12-29 15:00:16 +0100

    added gateway thats works with express-http-proxy

---
+++ gateway/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/medAppRESTAPI', proxy('http://localhost:4000'))
+app.use('/adminAPI', proxy('http://localhost:7000'))
+
+
+app.listen(3000, () => {
+    console.log('Gateway is Listening to Port 3000')
+})
